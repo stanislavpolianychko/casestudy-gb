1bc0bbc3b4d19bd2c1ac4f713cb0cbe4949baa5f
Stanislav Polianychko <staspolianychko@gmail.com>
2024-07-17 13:07:40 +0200

    refactor(AppConfig): renamed vars, logs

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,10 +6,9 @@ abstract class AppConfig {
    * If a required environment variable is not set, it logs an error message and exits the process.
    */
   public static load(): void {
-    // Load the person base API URL from the environment variable
     const apiBaseUrl: string | undefined = process.env.NEXT_PUBLIC_API_BASE_URL;
     if (!apiBaseUrl) {
-      console.error('Person base API URL is not set');
+      console.error('NEXT_PUBLIC_API_BASE_URL is not set in your .env file.');
       process.exit(1);
     }
     this.apiBaseUrl = apiBaseUrl;
